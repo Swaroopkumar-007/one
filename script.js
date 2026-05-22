605c08d23e2abe4944b374279cd4ddb98379bd38
Swaroopkumar-007 <swaroopkumaravala@gmail.com>
2026-05-22 14:28:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,4 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
   alert('Thank you for your message! We will get back to you soon.');
   this.reset();
 });
-//testing the webhook
+//testing the webhook testing it again
